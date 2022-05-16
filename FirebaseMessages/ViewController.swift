2fb589c45cfa15a15f317cbf67d945e4507aeb74
Alex Thompson <Alex@appNerd.com.au>
2022-05-16 18:37:29 +1000

    Initial Commit

---
+++ FirebaseMessages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseMessages
+//
+//  Created by Alexander Thompson on 16/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
